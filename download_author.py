73aea7d2bc218c9a8577a5b84a0f6343ad5f0eb1
Qizhou Wang <imjoewang@gmail.com>
2023-03-28 23:31:29 +0300

    added options : 1. update tag or not 2. randomnize tag or not 3. skip model check or not 4. Finished author batch download, adjusted waiiting time

--- download_author.py
+++ download_author.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 from selenium.webdriver.support import expected_conditions as EC
 import requests
@@ -7,254 +8,79 @@ import re
 import shutil
 import requests
 import json
+from pprint import pp
 from fake_useragent import UserAgent
 import pyperclip
-
-
-def mkdir(path, rm=False):
-    if not os.path.exists(path):
-        os.makedirs(path)
-    else:
-        if rm:
-            shutil.rmtree(path)
-            os.mkdir(path)
-
-
-cookies = {
-    '__stripe_mid': '1fe09fa5-502c-48ee-b58a-99a2b731d757f97286',
-    'f_period': 'Day',
-    'f_sort': 'Most%20Liked',
-    '__Secure-next-auth.callback-url': 'https%3A%2F%2Fcivitai.com',
-    '__Host-next-auth.csrf-token': 'd2074cb6dac098eaf78897c2a7db2035f4540a307eabb2cd9edc4355925245fc%7C2f3f0646877a1c71195f9756581e8035b714d5508a0bc0f077fc61e1b6e99c2f',
-    '__stripe_sid': '9f102a67-cfb3-4943-92b1-c80a81b58c618985fa',
-    '__Secure-civitai-token': 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..HSXwP625QH7flO7Y.4JIVR6cB437_gFapCmEN0EsJqafFpnlEjNrhymhsqeTUyH1asR2D_aaWLhv-Giba7s2KtVWGo32H54YaKPqe0bfJAgifwI5coGzIJIqJnZhG8zjS-U5N63ZepEKjVPzJQzs2gNXZb3M0jkqvdsjM3CvIWXFBG9F83opn3PYkFV9Nb-BD-IVhOdSfLk9UvjhytCKcRnRT_l22lBLYYVpqGmmF8xP3XlQ-lmQfOa7gsIRnoriCu4rh4rOAB8cxDeSVZ81RE4zPzr5bSgA6aHQo7FL9cZUMqBvFAdWbgR2JKZbLkk3R7U8XeGmLni6BuYEjX2fge5cXEqHzux9pV67wXk6Yqr95qtxsQXhb7RRuSZXbrrYrghDajLAXN52J5P-xXTUrQ3UiS_I1kuJQSHffoj5t4uClr6T_0oMxsVnir0RXgIKVWz450RhlnxU2arUVkcUzrjNXPIoUlXsdngzOx_jUIjxmIskOfKESXlDAYfS6_XoBea_hi9ZWixdib65C3XqnGqcuV8G0Wv1kWJpz-ymfcks4c2hRMwpn7bhl_GBCM7H-sBdeDM2X4Bhv6yM0vO0Csbd59rgDf4IQZZb5Oqd6BjdoXOLNlqbUDgFIL8oK3xaVeqrrSgN90qqDixYk8GgQgCrPalHAQmmVBL1Etuhfa91W3zcs_ft0r9LKAfw8XgcS6GDZQgIR3PjHFIrh0OypInWbtGVc3sY1_kEK0jIAnXwzuin5_4bp4ggbRqVwx5k24gQReDjrk4Hc7s_mBsDq7FgmAwqoXqXkkbgwH1UsDQdXac1pKmf1DqHYg021wJfzwNSW5jTkZPNs4B_FTedFsEDrLaepnSlRzK0fjvnt8s-1JXjmsXGtZeWdqprl8_lCXHZvQJ_XZCQN-3GesN4hEF-Ktk1tK4As7CkJrmSVaoQfNkJd20hp-ONCZ6wpZhc-cWSdey73EXzi1t4xbDGIiE7N6j_cEKvuteZLXj3d-Nh4LzjMGW7XNQ.Uqzcg1x3Eu08qMlrSc5hxg',
-}
-
-headers = {
-    'authority': 'civitai.com',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
-    'cache-control': 'max-age=0',
-    'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-}
-
-# url1 = 'https://civitai.com/models/23440/panty-pull-or-test-sex-act-lora-117'
-
-# url2='https://civitai.com/models/23885/yuri-cunnilingus'
-
-
-author_file = 'liked'
-
-with open(f'./src/{author_file}.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-soup = BeautifulSoup(html, 'html.parser')
-
-url2 = 'https://civitai.com/user/dpp12'
-
-url2 = 'https://civitai.com/models/24062/lora-or-landmine-girl-fashion-or'
-response = requests.get(
-    url2,
-    cookies=cookies,
-    headers=headers,
-)
-soup = BeautifulSoup(response.content, 'html.parser')
-
-frame = soup.select('.mantine-8od8ev')  # 框架
-
-gallery = soup.select('.mantine-w0udt3')  # 图片
-
-btn = soup.select('.mantine-xickm0')  # 18 按钮
-
-item_ls = soup.select('.mantine-cf0b3j')
-
-print(len(gallery))
-print(len(frame))
-print(len(btn))
-# print(img[0].get('href'))
-print(len(item_ls))
-
+from utils import mkdir, get_file_sha256, dl_file, init_driver, purge_dirname
 from selenium.webdriver.common.by import By
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-ua = UserAgent()
-userAgent = ua.random
-print(userAgent)
-
-# 创建 ChromeOptions 对象，并设置 headless 模式
-chrome_options = Options()
-chrome_options.add_argument(f'user-agent={userAgent}')
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('user-agent= "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"')
-driver = webdriver.Chrome(options=chrome_options)
-time.sleep((2))
-driver.get(url2)
-time.sleep((2))
-with open(os.path.join('cookies.json'), 'r') as f:
-    cookies = json.load(f)
-for item in cookies:
-    cookie = {'name': item['name'], 'value': item['value']}
-    driver.add_cookie(cookie)
-
-time.sleep((2))
-driver.get(url2)
-c = driver.get_cookies()
-print(c)
-time.sleep((5))
-
-# total_height = driver.execute_script("return document.body.scrollHeight")
-# driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-# time.sleep((2))
-
-
-##############################################################################################
-
-button_id = '.mantine-1d2keo9'
-cp_button_id = '.mantine-192n9t8'
-
-################### -------------------------------------------------------####################
-
-url = 'https://civitai.com/models/24062/lora-or-landmine-girl-fashion-or'
-driver.get(url)
-html = driver.page_source
-soup2 = BeautifulSoup(html, 'html.parser')
-# b = soup2.select(button_id)
-left_btn_id = '.mantine-oax1he'
-cp_gen_btn_id = 'mantine-cbbdk9'
-#btn = driver.find_elements(By.CLASS_NAME, 'mantine-1d2keo9')
-#img_click = driver.find_elements(By.CLASS_NAME, 'mantine-1d2keo9')
-gallery_id = 'mantine-w0udt3'
-
-## selenium method of gallery
-gallery_click = driver.find_elements(By.CLASS_NAME, gallery_id)
-gallery_ls = []
-for g in gallery_click:
-    href = g.get_attribute('href')
-    if href.startswith('https://civitai.com/gallery/'):
-        gallery_ls.append(href)
-
-## soup parse method of gallery
-#gallery_ls=soup2.select('.'+gallery_id)
-#g_url_ls=[]
-#for g in gallery_ls:
-#    href = g.get('href')
-#    if href.startswith('https://civitai.com/gallery/'):
-#        g_url_ls.append(href)
-
-
-# 打开某一个gallery
-driver.get(gallery_ls[0])
-soup2 = BeautifulSoup(driver.page_source, 'html.parser')
-
-# img_cache_name=‘mantine-1mslj4c
-# img_cache = soup2.select('.'+img_cache_name)
-# try:
-#    cache_url=img_cache[0].get('src')
-# except:
-#    print('no cache for this image')
-
-
-# 打开图片的链接
-cache_url = re.findall('https://imagecache.civitai.com+?(?=\")', str(soup2))[0]
-with requests.get(cache_url, stream=True) as r:
-    filename = '1.png'
-    with open(filename, 'wb') as f:
-        # for chunk in r.iter_content(chunk_size=8192):
-        # f.write(chunk)
-        shutil.copyfileobj(r.raw, f)
-
-import pyperclip
-
-## 寻找并点击generate按钮
-cp_gen_btn_id = 'mantine-cbbdk9'
-cp_gen_btn = driver.find_elements(By.CLASS_NAME, cp_gen_btn_id)[0]
-cp_gen_btn.click()
-
-## 生成标签
-with open('request_ver/tags1.txt', 'w', encoding='utf-8') as f:
-    tag = str(pyperclip.paste())
-    tag = tag.replace('\r', '')
-    tag = tag.replace('\n', '')
-    f.write(tag)
+import numpy as np
+import random
+import shutil
+from PIL import Image
+from PIL.PngImagePlugin import PngInfo
+from single_model import dl_model
+import argparse
 
 
 
-## 获取某一个作者主页下面所有的model
-url='https://civitai.com/user/Rerorerorero'
 
+class OPT (object):
+    pass
 
-ua = UserAgent()
-userAgent = ua.random
-print(userAgent)
+#opt = OPT()
+#opt.update_tag = True
+#opt.random_tag = True
+#opt.skip_model = True
+#opt.versions = 3
 
-# 创建 ChromeOptions 对象，并设置 headless 模式
-chrome_options = Options()
-chrome_options.add_argument(f'user-agent={userAgent}')
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('user-agent= "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"')
-driver = webdriver.Chrome(options=chrome_options)
-driver.maximize_window()
-time.sleep((0.5))
-driver.get(url)
-time.sleep((2))
-with open(os.path.join('cookies.json'), 'r') as f:
-    cookies = json.load(f)
-for item in cookies:
-    cookie = {'name': item['name'], 'value': item['value']}
-    driver.add_cookie(cookie)
+def dl_author(username, savedir,opt):
+    url = f'https://civitai.com/api/v1/models?username={username}'
+    response = requests.get(url, headers={"Content-Type": "application/json"})
+    info = response.json()
 
-time.sleep((5))
-
-
-def get_model_count(tt):
-    driver.get(url)
-    time.sleep((tt))
-    html = driver.page_source
-    soup2 = BeautifulSoup(html, 'html.parser')
-    frame = soup2.select('.mantine-8od8ev')  # 框架
-    gallery = soup2.select('.mantine-w0udt3')  # 图片
-    btn = soup2.select('.mantine-xickm0')  # 18 按钮
-    item_ls2 = soup2.select('.mantine-cf0b3j')
-    #print(len(gallery))
-    #print(len(frame))
-    #print(len(btn))
-    print(len(item_ls2))
-    # driver.quit()
-    info = soup2.select('.mantine-2qa0ve')
-
-    if len(info) > 5:
-        author_info = {
-            'Rank': info[0].get_text(),
-            'Uploads': info[2].get_text(),
-            'Followers': info[3].get_text(),
-            'Likes': info[4].get_text(),
-            'Downloads': info[5].get_text(),
-        }
-    elif len(info) == 5:
-        author_info = {
-            'Uploads': info[1].get_text(),
-            'Followers': info[2].get_text(),
-            'Likes': info[3].get_text(),
-            'Downloads': info[4].get_text(),
-        }
+    dl_count=1
+    if len(info['items']) == info['metadata']['totalItems']:
+        print(f'All {info["metadata"]["totalItems"]} models found, start downloading')
+        for model in info['items']:
+            print(f'Model {dl_count}: ', model['name'])
+            dl_model(model['id'], savedir=savedir, versions_num=opt.versions, update_tag=opt.update_tag,
+                     random_tag=opt.random_tag, skip_model=opt.skip_model)
+            dl_count+= 1
     else:
-        author_info = {}
+        for page in range(1,info['metadata']['pageSize']+1):
+            if page == 1:
+                print(f'Found {len(info["items"])} models in page {page}, start downloading')
+                for model in info['items']:
+                    print(f'Model {dl_count}: ', model['name'])
+                    dl_model(model['id'], savedir=savedir, versions_num=opt.versions, update_tag=opt.update_tag,
+                             random_tag=opt.random_tag, skip_model=opt.skip_model)
+                    dl_count += 1
+            else:
+                url = f'https://civitai.com/api/v1/models?username={username}&page={page}'
+                response = requests.get(url, headers={"Content-Type": "application/json"})
+                info = response.json()
+                for model in info['items']:
+                    print(f'Downloading model {dl_count}: ', model['name'])
+                    dl_model(model['id'], savedir=savedir, versions_num=opt.versions, update_tag=opt.update_tag,
+                             random_tag=opt.random_tag, skip_model=opt.skip_model)
+                    dl_count += 1
+    print(f'All {dl_count} models downloaded')
 
-    c = re.findall('/models/.{15}', str(item_ls2))
-    print('model count: ', len(c))
-    #print(c)
-    if eval(author_info['Uploads']) == len(c):
-        print('model count is correct')
-    #print(author_info)
 
-
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('-username', type=str,default='NONE')
+    args.add_argument('-update_tag', action='store_true')
+    args.add_argument('-random_tag', action='store_true')
+    args.add_argument('-skip_model', action='store_true')
+    args.add_argument('-versions', type=int, default=1)
+    args.add_argument('-savedir', type=str, default='./dl')
+    opt = args.parse_args()
+    if opt.username == 'NONE':
+        print('Please input the username')
+        sys.exit(-1)
+    else:
+        dl_author(opt.username, opt.savedir, opt)
 
--- single_model.py
+++ single_model.py
@@ -21,13 +21,11 @@ from PIL import Image
 from PIL.PngImagePlugin import PngInfo
 
 
-# driver=init_driver()
-##
 
 
 def dl_model(model_id, savedir='./dl', versions_num=1, update_tag=True, random_tag=False, skip_model=False):
-    # model_id = 12597
-    # savedir = './dl'
+    time.sleep(0.5)
+    # respect the api provides, do not request too fast
     response = requests.get(f"https://civitai.com/api/v1/models/{model_id}",
                             headers={"Content-Type": "application/json"})
     info = response.json()
@@ -37,11 +35,11 @@ def dl_model(model_id, savedir='./dl', versions_num=1, update_tag=True, random_t
     name = purge_dirname(name)
     dl_count = 0
     for model_latest in info['modelVersions']:
-        if dl_count == versions_num:
+        if dl_count == versions_num and not skip_model:
             break
         dl_dir = os.path.join(savedir, info['type'], creator + '_' + name, purge_dirname(model_latest['name']))
         mkdir(dl_dir)
-        mkdir(os.path.join(dl_dir, 'tags'))
+        mkdir(os.path.join(dl_dir, 'imgs'))
         metadata = {
             "Name": name,
             "Creator": creator,
@@ -55,13 +53,19 @@ def dl_model(model_id, savedir='./dl', versions_num=1, update_tag=True, random_t
             "Size": np.round(model_latest['files'][0]['sizeKB'] / 1024, 2),
             'Filename': model_latest['files'][0]['name'],
             "Download_url": model_latest['files'][0]['downloadUrl'],
-            "SHA256": model_latest['files'][0]['hashes']['SHA256'],
         }
+        try :
+            metadata['SHA256'] = model_latest['files'][0]['hashes']['SHA256']
+        except:
+            pass
 
         if not skip_model:
             with requests.get(metadata['Download_url'], stream=True) as r:
                 filename = os.path.join(dl_dir, metadata['Filename'])
-                if os.path.exists(filename):
+                if 'SHA256' not in metadata.keys():
+                    print(f'No SHA256 for {name}, download anyway')
+                    flag = True
+                elif os.path.exists(filename):
                     existing_hash = get_file_sha256(filename)
                     if existing_hash.capitalize() == metadata['SHA256'].capitalize():
                         print(f'File {filename} already exists')
@@ -69,7 +73,7 @@ def dl_model(model_id, savedir='./dl', versions_num=1, update_tag=True, random_t
                             dl_count += 1
                             continue
                         else:
-                            print(f'Update Tag of {name}')
+                            print(f'Update Tag of {name}: { model_latest["name"]}')
                             flag = False
                     else:
                         print(f'Update Model {filename}, size {metadata["Size"]} MB')
@@ -84,7 +88,7 @@ def dl_model(model_id, savedir='./dl', versions_num=1, update_tag=True, random_t
                         # f.write(chunk)
                         shutil.copyfileobj(r.raw, f)
         else:
-            print(f'Update Tag of {name}')
+            print(f'Update Tag of {name}: { model_latest["name"]}')
 
         with open(os.path.join(dl_dir, 'metadata.json'), 'w', encoding='utf-8') as f:
             json.dump(metadata, f, indent=4, ensure_ascii=False)
@@ -92,71 +96,69 @@ def dl_model(model_id, savedir='./dl', versions_num=1, update_tag=True, random_t
         with open(os.path.join(dl_dir, 'alldata.json'), 'w', encoding='utf-8') as f:
             json.dump(info, f, indent=4, ensure_ascii=False)
 
-        gallery = model_latest['images']
-        with_tag_ls = []
-        no_tag_ls = []
-        for g in gallery:
-            if g['meta'] is not None:
-                with_tag_ls.append(g)
-            else:
-                no_tag_ls.append(g)
+        if update_tag:
+            gallery = model_latest['images']
+            with_tag_ls = []
+            no_tag_ls = []
+            for g in gallery:
+                if g['meta'] is not None:
+                    with_tag_ls.append(g)
+                else:
+                    no_tag_ls.append(g)
 
-        if random_tag:
-            random.shuffle(with_tag_ls)
-            random.shuffle(no_tag_ls)
+            if random_tag:
+                random.shuffle(with_tag_ls)
+                random.shuffle(no_tag_ls)
 
-        residual_tag_num = np.min([len(no_tag_ls), 5 - np.min([len(with_tag_ls), 5])])
+            residual_tag_num = np.min([len(no_tag_ls), 5 - np.min([len(with_tag_ls), 5])])
 
-        for tag_idx, g in enumerate(with_tag_ls):
-            with requests.get(g['url'], stream=True) as r:
-                imgname = os.path.join(dl_dir, 'tags', f'tag{tag_idx}.png')
-                r.raise_for_status()
-                if os.path.exists(imgname):
-                    os.remove(imgname)
-                with open(imgname, 'wb') as f:
-                    # for chunk in r.iter_content(chunk_size=8192):
-                    # f.write(chunk)
-                    shutil.copyfileobj(r.raw, f)
-                if tag_idx == 4:
-                    break
+            for tag_idx, g in enumerate(with_tag_ls):
+                with requests.get(g['url'], stream=True) as r:
+                    imgname = os.path.join(dl_dir, 'imgs', f'tag{tag_idx}.png')
+                    r.raise_for_status()
+                    if os.path.exists(imgname):
+                        os.remove(imgname)
+                    with open(imgname, 'wb') as f:
+                        # for chunk in r.iter_content(chunk_size=8192):
+                        # f.write(chunk)
+                        shutil.copyfileobj(r.raw, f)
+                    if tag_idx == 4:
+                        break
 
-            msg = ''
-            msg += g['meta']['prompt'] + '\n'
-            try:
-                msg += 'Negative prompt: ' + g['meta']['negativePrompt'] + '\n'
-            except:
-                print('No negative prompt')
-            for key in ['Size', 'seed', 'Model', 'steps', 'sampler', 'cfgScale',
-                        'Model hash']:
+                msg = ''
+                msg += g['meta']['prompt'] + '\n'
                 try:
-                    msg += key + ': ' + str(g['meta'][key]) + ',  '
+                    msg += 'Negative prompt: ' + g['meta']['negativePrompt'] + '\n'
                 except:
-                    # print('this tag is missing:', key)
-                    pass
-            #png_info={'parameters':msg}
-            image = Image.open(imgname)
-            png_info = PngInfo()
-            png_info.add_text('parameters', msg)
-            image.save(imgname, pnginfo=png_info)
+                    print('No negative prompt')
+                for key in ['Size', 'seed', 'Model', 'steps', 'sampler', 'cfgScale',
+                            'Model hash']:
+                    try:
+                        msg += key + ': ' + str(g['meta'][key]) + ',  '
+                    except:
+                        # print('this tag is missing:', key)
+                        pass
+                #png_info={'parameters':msg}
+                image = Image.open(imgname)
+                png_info = PngInfo()
+                png_info.add_text('parameters', msg)
+                image.save(imgname, pnginfo=png_info)
 
 
-            # with open(os.path.join(dl_dir, 'tags', f'tag{tag_idx}.txt'), 'w', encoding='utf-8') as f:
-            # f.write(msg)
+            for tag_idx in range(residual_tag_num):
+                with requests.get(g['url'], stream=True) as r:
+                    filename = os.path.join(dl_dir, 'imgs', f'tag{tag_idx + len(with_tag_ls)}.png')
+                    r.raise_for_status()
+                    with open(filename, 'wb') as f:
+                        # for chunk in r.iter_content(chunk_size=8192):
+                        # f.write(chunk)
+                        shutil.copyfileobj(r.raw, f)
 
-        for tag_idx in range(residual_tag_num):
-            with requests.get(g['url'], stream=True) as r:
-                filename = os.path.join(dl_dir, 'tags', f'tag{tag_idx + len(with_tag_ls)}.png')
-                r.raise_for_status()
-                with open(filename, 'wb') as f:
-                    # for chunk in r.iter_content(chunk_size=8192):
-                    # f.write(chunk)
-                    shutil.copyfileobj(r.raw, f)
-
-        shutil.copyfile(os.path.join(dl_dir, 'tags', 'tag0.png'), os.path.join(dl_dir, 'cover.png'))
-        dl_count += 1
+            shutil.copyfile(os.path.join(dl_dir, 'imgs', 'tag0.png'), os.path.join(dl_dir, 'cover.png'))
+            dl_count += 1
 
     return
 
 
 if __name__ == '__main__':
-    dl_model(8484, savedir='./dl', versions_num=3, update_tag=True, random_tag=True, skip_model=False)
+    dl_model(11468, savedir='./dl', versions_num=3, update_tag=True, random_tag=True, skip_model=False)
